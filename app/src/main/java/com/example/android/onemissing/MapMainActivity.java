560a3dbfad8970fd5334a1f7aec6f54b0a824ca9
Pau <fcpau4@gmail.com>
2017-03-03 18:42:20 +0100

    Correct map visualization. Now building a new Activity to let user publish new events.

--- app/src/main/java/com/example/android/onemissing/MapMainActivity.java
+++ app/src/main/java/com/example/android/onemissing/MapMainActivity.java
@@ -1,16 +1,24 @@
 package com.example.android.onemissing;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
+
 
 public class MapMainActivity extends AppCompatActivity {
 
+    private ImageButton btEvent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,17 +26,20 @@ public class MapMainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btEvent = (ImageButton) findViewById(R.id.btEvent);
+        btEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), NewEventActivity.class);
+                startActivity(intent);
             }
         });
+
     }
 
-    @Override
+
+
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_map_main, menu);
@@ -48,5 +59,5 @@ public class MapMainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
--- app/src/main/java/com/example/android/onemissing/MapMainActivityFragment.java
+++ app/src/main/java/com/example/android/onemissing/MapMainActivityFragment.java
@@ -5,11 +5,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MapMainActivityFragment extends Fragment {
+
+    private double latitudeBCN = 41.390205;
+    private double longitudeBCN = 2.154007;
+    private MapView map;
+
 
     public MapMainActivityFragment() {
     }
@@ -17,6 +29,30 @@ public class MapMainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_map_main, container, false);
+
+        map = (MapView) view.findViewById(R.id.map);
+
+        //First, set map view options.
+        setMap();
+
+
+        return view;
+    }
+
+
+    /**
+     * This method configures the map visualization and gets a Controller to set a start point location.
+     */
+    public void setMap(){
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        GeoPoint startPoint = new GeoPoint(latitudeBCN, longitudeBCN);
+        mapController.setCenter(startPoint);
     }
 }
